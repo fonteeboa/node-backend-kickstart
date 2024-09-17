bd9df036cee58b51e094fb70447219c331743ade
João Victor <galvao845@gmail.com>
2024-09-17 19:07:11 -0300

    removendo arquivos testes e adicionando gitignore

--- projects/teste2/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    coverageDirectory: 'coverage',
-    collectCoverage: true,
-    coverageReporters: ['json', 'lcov', 'text', 'clover'],
-    testMatch: ['**/src/tests/**/*.test.js'],  // Mapeia todos os arquivos de teste dentro da pasta src/tests
-    verbose: true,
-};
