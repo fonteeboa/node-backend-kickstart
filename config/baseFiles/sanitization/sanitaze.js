b97cbd8d64a584cb78644e78536b51b5b27eb05c
João Victor <galvao845@gmail.com>
2024-10-24 12:55:46 -0300

    Sanitization: adjusting the sanitization process to allow more items while tightening restrictions on keywords and command sequences.

--- config/baseFiles/sanitization/sanitaze.js
+++ config/baseFiles/sanitization/sanitaze.js
@@ -5,9 +5,34 @@ const noSqlInjectionPattern = /(\$where|\$gt|\$lt|\$or)/gi;
 const xssPattern = /<script.*?>.*?<\/script>|<img.*?onerror=.*?>/gi;
 const headerInjectionPattern = /\r\n|\r|\n/g;
 const extendedAsciiPattern = /[\x80-\xFF]/g;
-const dangerousFunctionsPattern = /\b(eval|exec|system|spawn)\b/gi;
+const dangerousFunctionsPattern = /\b(eval|exec|system|spawn|whoami|etcpasswd|cat|touch|shutdown|uname)\b/gi;
 const xxePattern = /<\?xml[\s\S]*?<!DOCTYPE[\s\S]*?>[\s\S]*?<\/.*?>/g;
+const pathTraversalPattern = /(\.\.|\/\/|\\|\.\/)/g;
+const allowedCharsPattern = /[^a-zA-Z0-9\s@._\-,:<>[\]{}|]/g;
 const MAX_PAYLOAD_SIZE = 10000;
+
+const accentMapping = {
+  'Ё': 'E', 'ё': 'e', 'Є': 'E', 'є': 'e', 'Ї': 'I', 'ї': 'i', 'Љ': 'Lj', 'љ': 'lj', 'Њ': 'Nj', 'њ': 'nj',
+  'Ћ': 'C', 'ћ': 'c', 'Ќ': 'K', 'ќ': 'k', 'Ў': 'U', 'ў': 'u', 'Џ': 'Dz', 'џ': 'dz', 'Ә': 'A', 'ә': 'a', 'Ғ': 'G', 'ғ': 'g',
+  'Қ': 'K', 'қ': 'k', 'Ң': 'N', 'ң': 'n', 'Ү': 'U', 'ү': 'u', 'Ұ': 'U', 'ұ': 'u', 'Ҳ': 'H', 'ҳ': 'h', 'Җ': 'Zh', 'җ': 'zh',
+  'Ҹ': 'Ch', 'ҹ': 'ch', 'Ѡ': 'O', 'ѡ': 'o', 'Ѣ': 'E', 'ѣ': 'e', 'Ѥ': 'E', 'ѥ': 'e', 'Ѧ': 'A', 'ѧ': 'a', 'Ѩ': 'Ya', 'ѩ': 'ya',
+  'Ѫ': 'U', 'ѫ': 'u', 'Ѭ': 'Yu', 'ѭ': 'yu', 'Ѯ': 'Ks', 'ѯ': 'ks', 'Ѱ': 'Ps', 'ѱ': 'ps', 'Ѳ': 'F', 'ѳ': 'f', 'Ѵ': 'V', 'ѵ': 'v',
+  'Ѷ': 'V', 'ѷ': 'v', 'Ѹ': 'U', 'ѹ': 'u', 'Ѻ': 'O', 'ѻ': 'o', 'Ѽ': 'O', 'ѽ': 'o', 'Ѿ': 'O', 'ѿ': 'o', 'Ҁ': 'S', 'ҁ': 's',
+  '҂': 'S', '҃': 's', '҄': 'N', '҅': 'n', '҆': 'T', '҇': 't', '҈': 'Zh', '҉': 'zh', 'Ҋ': 'Ch', 'ҋ': 'ch', 'Ҍ': 'D', 'ҍ': 'd',
+  'Ҏ': 'R', 'ҏ': 'r', 'Ӏ': 'I', 'Ӂ': 'Zh', 'ӂ': 'zh', 'Ӄ': 'K', 'ӄ': 'k', 'Ӆ': 'L', 'ӆ': 'l', 'Ӈ': 'N', 'ӈ': 'n', 'Ӊ': 'Ng',
+  'ӊ': 'ng', 'Ӌ': 'Ch', 'ӌ': 'ch', 'Ӎ': 'M', 'ӎ': 'm', 'ӏ': 'I', 'Ӑ': 'A', 'ӑ': 'a', 'Ӓ': 'A', 'ӓ': 'a', 'Ӕ': 'Ae', 'ӕ': 'ae',
+  'Ә': 'A', 'ә': 'a', 'Ӗ': 'E', 'ӗ': 'e', 'Ғ': 'G', 'ғ': 'g', 'Ҕ': 'G', 'ҕ': 'g', 'Җ': 'Zh', 'җ': 'zh', 'Ҙ': 'Z', 'ҙ': 'z',
+  'Қ': 'K', 'қ': 'k', 'Ҝ': 'K', 'ҝ': 'k', 'Ҟ': 'K', 'ҟ': 'k', 'Ҡ': 'K', 'ҡ': 'k', 'Ң': 'N', 'ң': 'n', 'Ҥ': 'N', 'ҥ': 'n',
+  'Ү': 'U', 'ү': 'u', 'Ұ': 'U', 'ұ': 'u', 'Ҳ': 'H', 'ҳ': 'h', 'Җ': 'Zh', 'җ': 'zh', 'Ҵ': 'Ts', 'ҵ': 'ts', 'Ҷ': 'Ch', 'ҷ': 'ch',
+  'Ҹ': 'Ch', 'ҹ': 'ch', 'Һ': 'H', 'һ': 'h', 'Ҽ': 'Ts', 'ҽ': 'ts', 'Ҿ': 'Ts', 'ҿ': 'ts', 'Ӏ': 'I', 'Ӂ': 'Zh', 'ӂ': 'zh'
+};
+
+function replaceAccentedCharacters(input) {
+  return input.split('').map(char => {
+    if (char === '-') return '_';
+    return accentMapping[char] || char;
+  }).join('');
+}
 
 function sanitizeInput(input) {
   switch (typeof input) {
@@ -19,7 +44,7 @@ function sanitizeInput(input) {
       if (xxePattern.test(input)) {
         return 'Request Blocked';
       }
-      
+      input = replaceAccentedCharacters(input);
       input = input.replace(dangerousCommandsPattern, '');
       input = input.replace(controlCharsPattern, '');
       input = input.replace(sqlInjectionPattern, '');
@@ -27,9 +52,10 @@ function sanitizeInput(input) {
       input = input.replace(xssPattern, '');
       input = input.replace(headerInjectionPattern, '');
       input = input.replace(extendedAsciiPattern, '');
+      input = input.replace(allowedCharsPattern, '');
+      input = input.replace(pathTraversalPattern, '');
       input = input.replace(dangerousFunctionsPattern, '');
-      input = input.trim();
-      return input.replace(/[^a-zA-Z0-9 @.]/g, '');
+      return input.trim();
 
     case 'object':
       if (input === null) {
